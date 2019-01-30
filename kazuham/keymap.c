f703851147af32801a9238e36387c3bbf5e83721
KazuhaM <kloenpo@gmail.com>
2019-01-30 15:53:50 +0900

    English complete 1

--- kazuham/keymap.c
+++ kazuham/keymap.c
@@ -10,6 +10,7 @@ extern keymap_config_t keymap_config;
 
 #define KC_SEN  MO(1)
 #define KC_SJP  MO(3)
+
 //#define KC_MARK TT(4)
 //#define KC_NMAL TO(5)
 
@@ -29,53 +30,24 @@ enum {
     SMSKYTOUCH,
 //    MARK, //
  //   NMAL, //
+};
+enum custom_keycodes {
+//    JTOE,
+//    ETOJ,
 
- //   JTOE,
- //   ETOJ,
-
-    DI_UU,
-    DI_AI,
-    DI_OU,
-    DI_EI,
-    DI_UI,
-    DI_AU,
-    DI_OI,
-    DI_IU,
-
-    DN_UNN,
-    DN_ANN,
-    DN_ONN,
-    DN_INN,
-    DN_ENN,
-
-    DM_BRCT,
-    DM_BRCTL,
-
-    D_MID
+    D_MID= SAFE_RANGE
 };
 
 //diphthong
 #define KC_JTOE LT(0,KC_MHEN)
 #define KC_ETOJ LT(2,KC_HENK)
+//#define KC_TOE TO(0)
+//#define KC_TOJ TO(2)
+//#define KC_JTOE JTOE
+//#define KC_ETOJ ETOJ
+
 //#define KC_ETOJ KC_HENK | DF(2)
 
-#define KC_DUU DI_UU
-#define KC_DAI DI_AI
-#define KC_DOU DI_OU
-#define KC_DEI DI_EI
-#define KC_DUI DI_UI
-#define KC_DAU DI_AU
-#define KC_DOI DI_OI
-#define KC_DIU DI_IU
-
-#define KC_DUNN DN_UNN
-#define KC_DANN DN_ANN
-#define KC_DONN DN_ONN
-#define KC_DINN DN_INN
-#define KC_DENN DN_ENN
-
-#define KC_DBRCT DM_BRCT
-#define KC_DBRCTL DM_BRCTL
 
 #define KC_DMID D_MID
 
@@ -109,183 +81,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                                    `-------+--------+--------'    `-------+--------+--------'
   ),
    
-    [_MSKYTOUCH] = LAYOUT_kc(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     ESC , F2 , F3 , F4 , F5 , F6                , F7 , F8 , F9 , F10, F11, F12,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TAB ,COMM, H  , R  , M  , W                 , DUU, DAI, DOU, DOT, DEI,HOME,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     LSFT, Y  , S  , T  , K  , N                 , U  , A  , O  , I  , E  ,MINS,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     LCTL,LGUI, B  , W  , X  , DEL,JTOE     ,ETOJ,BSPC,DANN,DONN,DINN,DENN,END ,
-  //`----+----+----+----+----+----+----/    \----+----+----+----+----+----+----'
-                       LCTL, SPC, SJP         ,___, ENT, ___ 
-  //                  `----+----+----'        `----+----+----'
+    [_MSKYTOUCH] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_JTOE,          KC_ETOJ, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    KC_LGUI, LOWER,   KC_ENT,                    KC_SPC,  RAISE,   KC_LALT
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
     
     [_SMSKYTOUCH] = LAYOUT_kc(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     ESC , F2 , F3 , F4 , F5 , F6                , F7 , F8 , F9 , F10, F11, F12,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TAB ,SCLN, V  , J  , F  , Q                 , DUI, DAU, DOI,COLN, DIU,HOME,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     LSFT, B  , Z  , D  , G  , P                 ,DUNN,DANN,DONN,DINN,DENN,DMID,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     LCTL,LGUI, B  , W  , X  , DEL,JTOE     ,ETOJ,BSPC,DANN,DONN,DINN,DENN,END ,
-  //`----+----+----+----+----+----+----/    \----+----+----+----+----+----+----'
-                       LCTL, SPC, SJP         ,___, ENT, ___ 
-  //                  `----+----+----'        `----+----+----'
-  ),
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______, JTOE    ,          ETOJ   , _______, _______, _______, _______, _______, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                   _______, _______, _______
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  )
    
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-//    case JTOE:
-//      if (record->event.pressed) {
-//        // when keycode JTOE is pressed
-//        DF(0);
-//        SEND_STRING(SS_TAP(X_MHEN));
-//      } else {
-//        // when keycode JTOE is released
-//      }
-//      return true;
-//      break;
-        case DI_UU:
-      if (record->event.pressed) {
-        // when keycode DI_AI is pressed
-        SEND_STRING("uu");
-      } else {
-        // when keycode DI_AI is released
-      }
-      return true;
-      break;
-    case DI_AI:
-      if (record->event.pressed) {
-        // when keycode DI_AI is pressed
-        SEND_STRING("ai");
-      } else {
-        // when keycode DI_AI is released
-      }
-      return true;
-      break;
-    case DI_OU:
-      if (record->event.pressed) {
-        // when keycode DI_OU is pressed
-        SEND_STRING("ou");
-      } else {
-        // when keycode DI_OU is released
-      }
-      return true;
-      break;
-    case DI_EI:
-      if (record->event.pressed) {
-        // when keycode DI_EI is pressed
-        SEND_STRING("ei");
-      } else {
-        // when keycode DI_EI is released
-      }
-      return true;
-      break;
-    case DI_UI:
-      if (record->event.pressed) {
-        // when keycode DI_UI is pressed
-        SEND_STRING("ui");
-      } else {
-        // when keycode DI_UI is released
-      }
-      return true;
-      break;
-    case DI_AU:
-      if (record->event.pressed) {
-        // when keycode DI_AU is pressed
-        SEND_STRING("au");
-      } else {
-        // when keycode DI_AU is released
-      }
-      return true;
-      break;
-    case DI_OI:
-      if (record->event.pressed) {
-        // when keycode DI_OI is pressed
-        SEND_STRING("oi");
-      } else {
-        // when keycode DI_OI is released
-      }
-      return true;
-      break;
-    case DI_IU:
-      if (record->event.pressed) {
-        // when keycode DI_IU is pressed
-        SEND_STRING("iu");
-      } else {
-        // when keycode DI_IU is released
-      }
-      return true;
-      break;
-    case DN_UNN:
-      if (record->event.pressed) {
-        // when keycode DI_UNN is pressed
-        SEND_STRING("unn");
-      } else {
-        // when keycode DI_UNN is released
-      }
-      return true;
-      break;
-    case DN_ANN:
-      if (record->event.pressed) {
-        // when keycode DI_ANN is pressed
-        SEND_STRING("ann");
-      } else {
-        // when keycode DI_ANN is released
-      }
-      return true;
-      break;
-    case DN_ONN:
-      if (record->event.pressed) {
-        // when keycode DI_ONN is pressed
-        SEND_STRING("onn");
-      } else {
-        // when keycode DI_ONN is released
-      }
-      return true;
-      break;
-    case DN_INN:
-      if (record->event.pressed) {
-        // when keycode DI_INN is pressed
-        SEND_STRING("inn");
-      } else {
-        // when keycode DI_INN is released
-      }
-      return true;
-      break;
-    case DN_ENN:
-      if (record->event.pressed) {
-        // when keycode DI_ENN is pressed
-        SEND_STRING("enn");
-      } else {
-        // when keycode DI_ENN is released
-      }
-      return true;
-      break;
-    case DM_BRCT:
-      if (record->event.pressed) {
-        // when keycode DM_BRCT is pressed
-        SEND_STRING("()"SS_TAP(X_LEFT));
-      } else {
-        // when keycode DM_BRCT is released
-      }
-      return true;
-      break;
-    case DM_BRCTL:
-      if (record->event.pressed) {
-        // when keycode DM_BRCTL is pressed
-        SEND_STRING("[]"SS_TAP(X_LEFT));
-      } else {
-        // when keycode DM_BRCTL is released
-      }
-      return true;
-      break;
     case D_MID:
       if (record->event.pressed) {
         // when keycode D_MID is pressed
@@ -296,5 +123,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return true;
       break;
   }
-  return true;
+  return false;
 }
